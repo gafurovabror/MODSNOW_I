10c6f99805e24c8f5a3a58aef601544388d9e4f1
gafurovabror <60718473+gafurovabror@users.noreply.github.com>
2026-09-08 22:12:37 +0200

    Update modsnow.f90

--- modsnow.f90
+++ modsnow.f90
@@ -1,6 +1,7 @@
 Copyright (C) [2016] Dr. Abror Gafurov, GFZ Helmholtz Centre for Geosciences
 
 Licensed under AGPL-3.0-only (GNU AFFERO GENERAL PUBLIC LICENSE Version 3, 19 November 2007).
+https://spdx.org/licenses/AGPL-3.0-only.html
 
 Copyright (C) 2007 Free Software Foundation, Inc. <https://fsf.org/>
 
